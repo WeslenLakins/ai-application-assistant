f3012333fa8b2dc658e018097fe9b40630dea3dc
Weslen Lakins <weslen.lakins@outlook.com>
2023-10-29 15:22:47 -0400

    Connect to the Database

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
+// Connect to database
+connectDB()
+
+// Initialize express
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Routes
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API.' })
 })
 
-// Routes
 app.use('/api/users', require('./routes/userRoutes'))
 
 // Error Handler
