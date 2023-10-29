6feb8124424225a58321cf300ec2842805659d0d
Weslen Lakins <weslen.lakins@outlook.com>
2023-10-29 18:54:45 -0400

    Added Login User Functionality and JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
-// Bring in express-async-handler to handle errors, bcryptjs to hash passwords, and the User model
+// Bring in express-async-handler to handle errors, bcryptjs to hash passwords, jsonwebtoken to create tokens, and the User model.
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
 // @desc:     Register a new user.
@@ -40,6 +41,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -51,8 +53,30 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route:    /api/users/login
 // @access:   Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route')
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  // If the user exists and the password is correct, send back the user object. Otherwise, throw an error.
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid credentials.')
+  }
 })
+
+// Generate token function
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
 
 module.exports = {
   registerUser,
