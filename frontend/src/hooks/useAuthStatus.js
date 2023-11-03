1479f87cdfeb5ddf4d8d8abae469b46553fa0679
Weslen Lakins <weslen.lakins@outlook.com>
2023-11-03 00:49:21 -0400

    Fixed private route 2x

--- frontend/src/hooks/useAuthStatus.js
+++ frontend/src/hooks/useAuthStatus.js
@@ -5,7 +5,7 @@ export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false)
   const [checkingStatus, setCheckingStatus] = useState(true)
 
-  const { user } = useSelector((state) => state.user || { user: null })
+  const { user } = useSelector((state) => state.auth)
 
   useEffect(() => {
     if (user) {
