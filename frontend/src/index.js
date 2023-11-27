400b55b225bbd4158fca5077e9c5d39f8674c626
rajmandaviya <mandaviya_rajesh@ymail.com>
2023-11-27 14:41:26 +0530

    feat: fix profile update issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,28 @@
 // Entry point of a React application that uses Redux for state management and the new React 18s createRoot API for concurrent rendering. Several modules are imported at the top of the file. These include React, createRoot from react-dom/client, Provider from react-redux, store from ./app/store, App from ./App, reportWebVitals from ./reportWebVitals, and ./index.css for global styles. The container constant is a reference to the DOM element with the id of 'root'. This is where the React application will be attached. The root constant is created by calling createRoot with the container as an argument. This sets up a root for concurrent rendering, a new feature in React 18. The root.render method is called with a JSX structure as an argument. This structure represents the root of the React application. Inside the JSX structure, React.StrictMode is used to wrap the application. This is a development tool that helps highlight potential problems in an application. The Provider component from react-redux is used to make the Redux store available to the rest of the app. The store prop is set to the store imported from ./app/store. The App component is included inside the Provider. This is the main component of the application. Finally, the reportWebVitals function is called. This function is used to measure and report on various aspects of your app's performance. This code sets up a React application with Redux for state management, concurrent rendering, and performance reporting.
 
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './app/store'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { persistor, store } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
-const container = document.getElementById('root')
-const root = createRoot(container)
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
