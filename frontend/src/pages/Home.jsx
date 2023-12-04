c29015db95ffda3d74f4d9b8715de887e98ffa7a
Weslen Lakins <weslen.lakins@outlook.com>
2023-12-03 21:04:48 -0500

    Add useState import to Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,6 +15,7 @@ The code below is a React component named Home. This component appears to be the
 */
 
 import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
 import {
   FaLaptop,
   FaListAlt,
