42aadaf29cb06308125d656fec3ce574c486253d
Weslen Lakins <weslen.lakins@outlook.com>
2024-07-31 18:25:33 -0400

    chore: Update contact page UI and responsive styles

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,45 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
-  return (
-    <div className='contact-container'>
-      <section className='contact-section'>
-        <h1 className='contact-title'>Support</h1>
-        <div className='contact-info'>
-          <p>
-            <strong>Name:</strong> Weslen Lakins
-          </p>
-          <p>
-            <strong>Email:</strong> weslen.lakins@aiapplicationassistant.com
-          </p>
-          <p>
-            <strong>Phone:</strong> +1 865 679 6001
-          </p>
-          <p>
-            <strong>Address:</strong> 7873 Bahia Vista Ct, Jacksonville, FL
-            32256
-          </p>
-        </div>
-      </section>
-      <section className='contact-section'>
-        <h1 className='contact-title'>Press</h1>
-        <div className='contact-info'>
-          <p>
-            <strong>Name:</strong> Tammy Lakins
-          </p>
-          <p>
-            <strong>Email:</strong> tammy.lakins@aiapplicationassistant.com
-          </p>
-          <p>
-            <strong>Phone:</strong> +1 865 688 0770
-          </p>
-          <p>
-            <strong>Address:</strong> 7322 Old Clinton Pike, Knoxville, TN 37921
-          </p>
-        </div>
-      </section>
-    </div>
-  )
-}
+	return (
+		<div className='contact-container'>
+			<section className='contact-section'>
+				<h1 className='contact-title'>Founder, CEO</h1>
+				<div className='contact-info'>
+					<p>
+						<strong>Name:</strong> Weslen Lakins
+					</p>
+					<p>
+						<strong>Email:</strong> weslen.lakins@aiapplicationassistant.com
+					</p>
+				</div>
+			</section>
+			<section className='contact-section'>
+				<h1 className='contact-title'>Support</h1>
+				<div className='contact-info'>
+					<p>
+						<strong>Name:</strong> Tammy Lakins
+					</p>
+					<p>
+						<strong>Email:</strong> tammy.lakins@aiapplicationassistant.com
+					</p>
+				</div>
+			</section>
+		</div>
+	);
+};
 
-export default Contact
+export default Contact;
